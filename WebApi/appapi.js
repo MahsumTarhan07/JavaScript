52a7aeef5d8cc512c97fc80008b85fc4c1f462f9
MahsumTarhan07 <mahsumtarhan1@gmail.com>
2023-05-08 21:39:12 +0300

    Ajax-Web-Api-Examples

---
+++ WebApi/appapi.js
@@ -0,0 +1,15 @@
+
+const myElement = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    myElement.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  myElement.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
